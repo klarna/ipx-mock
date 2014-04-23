4f57eb94a3fc4575e62c803b2e5c21a77939781a
Nadav Arbel <nadav.arbel@klarna.com>
2014-04-23 11:47:07 +0300

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IpxMock::Application.config.secret_key_base = '2d8586b87fba8308a7dbe84b87ec234b8f35fd2349d4f5718413c093099e9f6d58382697c894e07f73db4574e740a8f6e817890afefb093ed1e1629b463e68b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IpxMock::Application.config.session_store :cookie_store, key: '_ipx-mock_session'
