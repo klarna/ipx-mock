c7bae3979658274a9d119eebacfc7bd2e3d429b7
Eyal Luxenburg <eyal.luxenburg@klarna.com>
2014-05-04 14:23:21 +0300

    typo

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
 
-  scope :to,           -> (to)    { where(:to => to) }
-  scope :created_after -> (after) { where("created_at >= '#{after}'") }
+  scope :to,            -> (to)    { where(:to => to) }
+  scope :created_after, -> (after) { where("created_at >= '#{after}'") }
 
 end
